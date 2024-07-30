d74d16fcc03b66a4ad4579037909cb3c3ee3c9f7
Vitalii Bilyi <vbilyi@ironscales.com>
2024-07-30 14:48:03 +0100

    fix

--- routes/test.route.js
+++ routes/test.route.js
@@ -3,7 +3,7 @@ const { SuccessResponseObject } = require('../common/http');
 
 const r = Router();
 
-r.get('/', (req, res) => {
+r.post('/', (req, res) => {
     console.log(req.data)
     res.json(new SuccessResponseObject(data=req.data));
 });
